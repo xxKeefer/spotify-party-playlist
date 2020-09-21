6984b44929239cafb834bf34111982bee0c0ab79
SimoSultan <simosultan2020@gmail.com>
2020-09-21 12:54:30 +1000

    modular show loading gif working

--- src/js/frontEnd.js
+++ src/js/frontEnd.js
@@ -1,10 +1,24 @@
 
+// import { showLoadingGif } from './helpers.js'
 
+// HTML ELEMENTS
 let addUserIdButton = document.getElementById('addUserIdButton')
 addUserIdButton.addEventListener('click', addUserIdInput)
 
 let generatePlaylistButton = document.getElementById('generatePlaylistButton')
 generatePlaylistButton.addEventListener('click', generatePlaylist)
+
+
+
+// DATA
+const data = [
+  {track: 'Never Gonna Give You Up', artist: 'Rick Astley', album: 'Whenever You Need Somebody', year: '1987'}, 
+  {track: 'Underwear Goes Inside the Pants', artist: 'Lazyboy', album: 'Lazyboy TV', year: '2004'}, 
+]
+
+
+
+// FUNCTIONS
 
 function addUserIdInput() {
   let inputCont = document.getElementById('input-cont')
@@ -14,15 +28,40 @@ function addUserIdInput() {
   inputCont.appendChild(input)
 }
 
+
+
 function removeUserIdInput() {
 
 }
 
+
+
 function generatePlaylist() {
-  let loadingGif = document.getElementById('loadingPlaylistGif')
-  
-  loadingGif.style.display = "block"
-  setTimeout(() => {
-    loadingGif.style.display = "none"
-  }, 2000);
+  showLoadingGif('loadingPlaylistGif', 2000)
+  showPlaylist()
+
 }
+
+
+
+
+function showElement(el) {
+  el.style.display = "block"
+}
+
+function hideElement(el) {
+  el.style.display = "none"
+}
+
+function showLoadingGif(id, time) {
+  let loadingGif = document.getElementById(id)
+  showElement(loadingGif)
+  setTimeout(() => {
+    hideElement(loadingGif)
+  }, time);
+}
+
+
+
+
+
