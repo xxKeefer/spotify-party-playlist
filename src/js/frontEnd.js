f7b02f2150687880b985fd12a1025a6b431d6c22
SimoSultan <simosultan2020@gmail.com>
2020-09-21 13:27:55 +1000

    playlist generation working

--- src/js/frontEnd.js
+++ src/js/frontEnd.js
@@ -28,38 +28,83 @@ function addUserIdInput() {
   inputCont.appendChild(input)
 }
 
+// function removeUserIdInput() {
+
+// }
 
 
-function removeUserIdInput() {
 
+function generateList(data) {
+  let list = document.getElementById('playlist-list')
+
+  data.forEach(element => {
+    let item = document.createElement('li')
+    item.classList.add('list-group-item', 'row')
+    
+    let img = document.createElement('img')
+    img.style = "width: 9%;"
+    img.classList.add('col-2')
+    img.style.display = 'inline-block'
+    img.src = 'img/music_note.png'
+    img.alt = ""
+
+    let text = document.createElement('p')
+    text.classList.add('col-10')
+    text.style.display = 'inline-block'
+    text.innerHTML = `<strong>${element.artist}:</strong> ${element.track}`
+    text.style.margin = 0
+    
+    item.appendChild(img)
+    item.appendChild(text)
+
+    list.appendChild(item)
+  });
 }
-
-
 
 function generatePlaylist() {
-  showLoadingGif('loadingPlaylistGif', 2000)
-  showPlaylist()
 
-}
+  let timeout = 2000
 
-
-
-
-function showElement(el) {
-  el.style.display = "block"
-}
-
-function hideElement(el) {
-  el.style.display = "none"
-}
-
-function showLoadingGif(id, time) {
-  let loadingGif = document.getElementById(id)
-  showElement(loadingGif)
+  document.getElementById('right-cont-header').textContent = "Getting Your Playlist"
+  showElement('loadingPlaylistGif')
+  hideElement('playlist-cont')
+  generateList(data)
+  
   setTimeout(() => {
-    hideElement(loadingGif)
-  }, time);
+    document.getElementById('right-cont-header').textContent = "Success!!"
+    hideElement('loadingPlaylistGif')
+    showElement('playlist-cont')
+}, timeout);
 }
+
+
+
+
+
+function showElement(id) {
+  document.getElementById(id).style.display = "block"
+}
+
+function hideElement(id) {
+  document.getElementById(id).style.display = "none"
+}
+
+
+// function showElement(el) {
+//   el.style.display = "block"
+// }
+
+// function hideElement(el) {
+//   el.style.display = "none"
+// }
+
+// function showLoadingGif(id, time) {
+//   let loadingGif = document.getElementById(id)
+//   showElement(loadingGif)
+//   setTimeout(() => {
+//     hideElement(loadingGif)
+//   }, time);
+// }
 
 
 
--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,29 +1,29 @@
 
 
-String.prototype.capitalize = function() {
-  return this.charAt(0).toUpperCase() + this.slice(1)
-}
+// String.prototype.capitalize = function() {
+//   return this.charAt(0).toUpperCase() + this.slice(1)
+// }
 
-export function showLoadingGif(id, time) {
-  let loadingGif = document.getElementById(id)
+// export function showLoadingGif(id, time) {
+//   let loadingGif = document.getElementById(id)
 
-  // showElement(loadingGif)
-  loadingGif.style.display = "block"
+//   // showElement(loadingGif)
+//   loadingGif.style.display = "block"
 
-  setTimeout(() => {
-    // hideElement(loadingGif)
-    loadingGif.style.display = "none"
-  }, time);
-}
-
-
-function showElement(el) {
-  return el.style.display = "block"
-}
-
-function hideElement(el) {
-  return el.style.display = "none"
-}
+//   setTimeout(() => {
+//     // hideElement(loadingGif)
+//     loadingGif.style.display = "none"
+//   }, time);
+// }
 
 
-export { showLoadingGif }
+// function showElement(el) {
+//   return el.style.display = "block"
+// }
+
+// function hideElement(el) {
+//   return el.style.display = "none"
+// }
+
+
+// // export { showLoadingGif }
